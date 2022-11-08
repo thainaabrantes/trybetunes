4caa34cb624c0c3d471fa272c022284e543792a3
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-11-08 14:14:23 -0300

    Corrige lint

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Loading extends Component {
-    render() {
-        return (
-            <p>Carregando...</p>
-        )
-    }
+  render() {
+    return (
+      <p>Carregando...</p>
+    );
+  }
 }
 
 export default Loading;
