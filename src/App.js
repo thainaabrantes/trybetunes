51a603c59688bedff5393bf96afa689ffdc760fd
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-11-08 14:11:48 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import Content from './components/Content';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Content />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </div>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,82 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    loginName: '',
+    isLoginButtonDisabled: true,
+    isLoading: false,
+    logged: false,
+  };
+
+  enableLoginButton = () => {
+    const { loginName } = this.state;
+    const maxLength = 3;
+    const loginNameInvalide = loginName.length < maxLength;
+
+    if (loginNameInvalide) {
+      this.setState({ isLoginButtonDisabled: true });
+    } else {
+      this.setState({ isLoginButtonDisabled: false });
+    }
+  };
+
+  onInputChange = ({ name, value }) => {
+    this.setState({ [name]: value }, () => {
+      this.enableLoginButton();
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { loginName } = this.state;
+    this.setState(
+      { isLoading: true },
+      async () => {
+        await createUser({ name: loginName });
+        this.setState({
+          isLoading: false,
+          logged: true,
+        });
+      },
+    );
+  };
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const {
+      loginName,
+      isLoginButtonDisabled,
+      isLoading,
+      logged,
+    } = this.state;
+
+    return (
+      <div data-testid="page-login">
+        {isLoading
+          ? <Loading />
+          : (
+            <form onSubmit={ this.handleSubmit }>
+              <input
+                data-testid="login-name-input"
+                type="text"
+                name="loginName"
+                value={ loginName }
+                onChange={ ({ target }) => this.onInputChange(target) }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="submit"
+                disabled={ isLoginButtonDisabled }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        { logged && <Redirect to="/search" /> }
+      </div>
+    );
   }
 }
 
