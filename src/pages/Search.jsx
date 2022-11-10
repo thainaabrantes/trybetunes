fb8b8480735c170a484b7dd26dfc30078d1cfbce
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-11-10 20:55:25 -0300

    Implementa requisição para pesquisar albuns

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends Component {
   state = {
     artistName: '',
+    artistAlbums: [],
     isSearchButtonDisabled: true,
+    isLoading: false,
+    succesMessage: '',
   };
 
   enableSearchButton = () => {
@@ -25,13 +31,37 @@ class Search extends Component {
     });
   };
 
+  searchAlbums = (event) => {
+    event.preventDefault();
+    const { artistName } = this.state;
+    this.setState(
+      { isLoading: true },
+      async () => {
+        const albumsResult = await searchAlbumsAPI(artistName);
+        this.setState({
+          artistAlbums: albumsResult,
+          succesMessage: `Resultado de álbuns de: ${artistName}`,
+          artistName: '',
+          isLoading: false,
+        });
+      },
+    );
+  };
+
   render() {
-    const { artistName, isSearchButtonDisabled } = this.state;
+    const {
+      artistName,
+      artistAlbums,
+      isSearchButtonDisabled,
+      isLoading,
+      succesMessage,
+    } = this.state;
 
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
+        {isLoading && <Loading />}
+        <form onSubmit={ this.searchAlbums }>
           <input
             data-testid="search-artist-input"
             type="text"
@@ -48,6 +78,24 @@ class Search extends Component {
             Pesquisar
           </button>
         </form>
+        <div>
+          {artistAlbums.length ? <p>{ succesMessage }</p> : <p />}
+          {artistAlbums.length
+            ? artistAlbums.map((album) => (
+              <div key={ album.collectionId }>
+                <img src={ album.artworkUrl100 } alt="Foto do album" />
+                <h1>{ album.collectionName }</h1>
+                <p>{album.artistName}</p>
+                <Link
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                  to={ `/album/${album.collectionId}` }
+                >
+                  ACESSAR ALBUM
+                </Link>
+              </div>
+            ))
+            : <p>Nenhum álbum foi encontrado</p>}
+        </div>
       </div>
     );
   }
