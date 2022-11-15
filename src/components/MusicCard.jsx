d6317cd8e36746b612447cf5fa0b01ace2e510ed
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-11-14 21:46:10 -0300

    Valida propTypes e corrige erros do eslint

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MusicCard extends Component {
-    render() {
-      const { previewUrl, trackName } = this.props;
-        return (
-            <section>
-                <p>{ trackName }</p>
-                <audio data-testid="audio-component" src={previewUrl} controls>
-                    <track kind="captions" />
-                    O seu navegador não suporta o elemento{" "} <code>audio</code>.
-                </audio>
-            </section>
-        )
-    }
+  render() {
+    const { previewUrl, trackName } = this.props;
+    return (
+      <section>
+        <p>{ trackName }</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </section>
+    );
+  }
 }
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
@@ -52,5 +53,13 @@ class Album extends Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
